fdfd3dcdde4669567fe6a7784f0e0445827e3cea
Kyrillos Gaitanis <kyrillos.gait@gmail.com>
2018-11-07 16:05:25 +0200

    AndroidX migration.

--- app/src/main/java/com/kyril/gymondotest/ui/main/MainActivity.kt
+++ app/src/main/java/com/kyril/gymondotest/ui/main/MainActivity.kt
@@ -1,7 +1,8 @@
-package com.kyril.gymondotest
+package com.kyril.gymondotest.ui.main
 
-import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import com.kyril.gymondotest.R
 
 class MainActivity : AppCompatActivity() {
 
